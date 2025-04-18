196898a275fdb4af123a69e353c16a3bc736ff70
Lingam <kjothilingam06@gmail.com>
2025-04-18 14:42:34 +0530

    SESSION DONE

--- src/app/interceptors/jwt.interceptor.ts
+++ src/app/interceptors/jwt.interceptor.ts
@@ -1,17 +1,35 @@
+// jwt.interceptor.ts
+import { inject } from '@angular/core';
 import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpErrorResponse } from '@angular/common/http';
+import { AuthService } from '../Services/auth.service';
+import { Router } from '@angular/router';
+import { throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 export const jwtInterceptor: HttpInterceptorFn = (req, next) => {
-  const token = localStorage.getItem('jwtToken'); // or sessionStorage.getItem
+  const authService = inject(AuthService);
+  const router = inject(Router);
 
-  if (token) {
-    const clonedReq = req.clone({
-      setHeaders: {
-        Authorization: `Bearer ${token}`
+  const token = localStorage.getItem('jwtToken');
+
+  const clonedReq = token
+    ? req.clone({
+        setHeaders: {
+          Authorization: `Bearer ${token}`
+        }
+      })
+    : req;
+
+  return next(clonedReq).pipe(
+    catchError((error: HttpErrorResponse) => {
+      if (error.status === 401 || error.status === 403) {
+        const message = error.error?.error || 'Session expired. Please login again.';
+        alert(message);
+        authService.logout();
+        router.navigate(['/login']);
       }
-    });
-    return next(clonedReq);
-  }
-  
-
-  return next(req);
+      return throwError(() => error);
+    })
+  );
 };
