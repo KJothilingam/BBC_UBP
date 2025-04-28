c17bc3934753111c909cba6a2ab1c277ddf48883
Lingam <kjothilingam06@gmail.com>
2025-04-28 12:36:24 +0530

    update

--- src/app/interceptors/jwt.interceptor.ts
+++ src/app/interceptors/jwt.interceptor.ts
@@ -23,12 +23,12 @@ export const jwtInterceptor: HttpInterceptorFn = (req, next) => {
 
   return next(clonedReq).pipe(
     catchError((error: HttpErrorResponse) => {
-      // if (error.status === 401 || error.status === 403) {
-      //   const message = error.error?.error || 'Session expired. Please login again.';
-      //   alert(message);
-      //   authService.logout();
-      //   router.navigate(['/login']);
-      // }
+      if (error.status === 401 || error.status === 403) {
+        const message = error.error?.error || 'Session expired. Please login again.';
+        alert(message);
+        authService.logout();
+        router.navigate(['/login']);
+      }
       return throwError(() => error);
     })
   );
